4ccb7bc67b53760ab20a4464a1212e798e914f65
shubhampatel2610 <shubhu2610@gmail.com>
2026-02-17 00:17:11 +0530

    commit for adding app logo

--- app/components/HeaderComponent/Header.tsx
+++ app/components/HeaderComponent/Header.tsx
@@ -1,12 +1,13 @@
+import AppLogo from "@/public/AppLogo";
 import Link from "next/link";
 
 const Header = () => {
   return (
     <>
       <nav className="w-full fixed top-0 left-0 bg-background/10 backdrop-blur-xl z-20 border-b">
-        <div className="mx-w-7xl mx-auto px-5 py-4 flex items-center justify-between">
+        <div className="mx-w-7xl mx-auto px-5 py-2.5 flex items-center justify-between">
           <Link href="/" className="text-2xl font-bold">
-            <div>EventSnap</div>
+            <div><AppLogo /></div>
           </Link>
         </div>
       </nav>
