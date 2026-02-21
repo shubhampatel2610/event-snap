7aa7a21b8d87d76725de8bdff4f011a0d5e0ea17
shubhampatel2610 <shubhu2610@gmail.com>
2026-02-21 17:00:49 +0530

    initial commit for landing page

---
+++ app/components/LandingPageComponent/LandingPageComponent.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { AppConstants } from '@/app/constants/AppConstants'
+import InputButton from '../FormComponents/ButtonComponent/InputButton'
+
+const LandingPageComponent = () => {
+
+    return (
+        <div className="grid grid-cols-12 min-h-screen">
+            <div className="col-span-12 md:col-span-6 px-15 py-5">
+                <div className="flex flex-col gap-10">
+                    <div>
+                        <h1 className="text-4xl sm:text-5xl md:text-5xl font-bold">
+                            {AppConstants.LANDING_PAGE_HEADER}
+                        </h1>
+                    </div>
+                    <div className="flex flex-col gap-7">
+                        <div>
+                            {AppConstants.LANDING_PAGE_TITLE.map((line, index) => (
+                                <span key={index} className={`text-3xl sm:text-3xl md:text-4xl font-semibold 
+              ${AppConstants.HIGHLIGHT_INDEXES.includes(index)
+                                        ? "bg-gradient-to-r from-[#8B5CF6] via-[#06B6D4] to-purple-400 bg-clip-text text-transparent"
+                                        : "text-[#ececec]"}`}
+                                >
+                                    {line}{" "}
+                                </span>
+                            ))}
+                        </div>
+                        <span className="text-1xl text-[#acacac]">
+                            {AppConstants.LANDING_PAGE_DESCRIPTION}
+                        </span>
+                        <div>
+                            <InputButton
+                                label="Get Started"
+                                size="lg"
+                                className={"rounded-full"}
+                                navigateTo={"/events"}
+                                variant={"secondary"}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-span-12 md:col-span-6 px-10 py-5">
+                Right Section
+            </div>
+        </div>
+    )
+}
+
+export default LandingPageComponent;
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,5 @@
-"use client";
-
-import { decrement, increment } from "./store/dashboardSlice";
-import { useAppDispatch, useAppSelector } from "./store/store";
+import LandingPageComponent from "./components/LandingPageComponent/LandingPageComponent";
 
 export default function Home() {
-  const dispatch = useAppDispatch();
-  const val = useAppSelector((state) => state.dashboard.value);
-
-  return (
-    <>
-      Hello World<div>{val}</div>
-      <button onClick={() => dispatch(increment())}>Click Me</button>
-      <button onClick={() => dispatch(decrement())}>Click Me</button>
-    </>
-  );
+  return <LandingPageComponent />;
 }
