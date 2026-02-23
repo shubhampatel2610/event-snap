5df2ba633ee3918bf2850b9e6bca1a72b142c3d2
shubhampatel2610 <shubhu2610@gmail.com>
2026-02-24 01:06:05 +0530

    commit - state country validation for slug

--- app/utils/helperFunctions.ts
+++ app/utils/helperFunctions.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { AppConstants } from "../constants/AppConstants";
+import { City, State } from "country-state-city";
 
 // Get category by ID
 export const getCategoryById = (id: any) => {
@@ -24,4 +25,36 @@ export const createLocationSlug = (city: string, state: string) => {
     const stateSlug = state.toLowerCase().replace(/\s+/g, '-');
 
     return `${citySlug}-${stateSlug}`;
+}
+
+// parse slug to get ciry and state
+export const getLocationDataFromSlug = (slug: string | null) => {
+    if (!slug) {
+        return { city: null, state: null, validSlug: false };
+    }
+
+    const slugArr = slug.split("-");
+
+    const cityName = slugArr[0].charAt(0).toUpperCase() + slugArr[0].slice(1);
+    const stateName = slugArr.slice(1).map((str) => str.charAt(0).toUpperCase() + str.slice(1)).join(" ");
+
+    // get all states of India and validate
+    const allStates = State.getStatesOfCountry("IN");
+
+    const stateExist = allStates.find((item: any) => item.name.toLowerCase() === stateName.toLowerCase());
+
+    if (!stateExist) {
+        return { city: null, state: null, validSlug: false };
+    }
+
+    // get all cities of that state and validate
+    const allCities = City.getCitiesOfState("IN", stateExist.isoCode);
+
+    const cityExist = allCities.find((item: any) => item.name.toLowerCase() === cityName.toLowerCase());
+
+    if (!cityExist) {
+        return { city: null, state: null, validSlug: false };
+    }
+
+    return { city: cityName, state: stateName, validSlug: true };
 }
