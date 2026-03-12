18175301c4e35f7d02cdfcdb90fc0cf50b5f4ae4
shubhampatel2610 <shubhu2610@gmail.com>
2026-03-13 00:16:36 +0530

    commit - landing page improvement

--- app/components/FooterComponent/Footer.tsx
+++ app/components/FooterComponent/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="border-t bg-gray-900 py-8 px-6 flex flex-col items-center gap-4">
+    <footer className="border-t bg-gray-900 py-5 px-6 flex flex-col items-center gap-1">
       <div className="text-sm text-gray-400">
         Made with ❤️ by Shubham P
       </div>
